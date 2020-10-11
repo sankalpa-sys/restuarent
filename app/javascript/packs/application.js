4b5d4b58a404efe2ed69c5bc90bc9d2923ce4c29
Sankalpa Neupane <sankalpaneupane@Sankalpas-MacBook-Pro.local>
2020-10-11 18:58:50 +0545

    trying

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,11 +3,14 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 import "bootstrap"
+import "@fortawesome/fontawesome-free/css/all";
+import "@fortawesome/fontawesome-free/js/all";
+
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-import "@fortawesome/fontawesome-free/js/all";
+
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
