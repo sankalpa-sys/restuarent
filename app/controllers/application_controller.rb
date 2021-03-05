078facfcd7402b8142994bbed408bf3b500e3013
Sankalpa Neupane <sankalpaneupane@Sankalpas-MacBook-Pro.local>
2021-03-05 18:42:10 +0545

    password confirmation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   protected
 
        def configure_permitted_parameters
-            devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:username,:firstname, :lastname, :email, :phone, :password, :admin)}
+            devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:username,:firstname, :lastname, :email, :phone, :password,:password_confirmation, :admin)}
 
             devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:username,:firstname, :lastname, :email, :phone, :password, :current_password)}
        end
